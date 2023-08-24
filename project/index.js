eae4cb63f940985350fa0e1011ff152fddf865d2
snigers <educon@yandex.ru>
2023-08-24 10:50:01 +0300

    video-2

---
+++ project/index.js
@@ -0,0 +1,6 @@
+const {randomInt, randomArr} = require('./random');
+
+
+console.log(randomInt());
+console.log(randomArr());
+// console.log(randomInt(1000, 1500));
---
+++ project/random/index.js
@@ -0,0 +1,7 @@
+const rand = require('./random_int');
+const randArr = require('./random_array');
+
+module.exports = {
+    "randomInt": rand,
+    "randomArr": randArr,
+};
